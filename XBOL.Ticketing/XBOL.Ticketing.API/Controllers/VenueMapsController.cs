cacff84d7b812ff55dc5d9a47ffea763fee6cd3c
Roberto Galvan <roberto.galvan@odasoft.com.mx>
2026-03-02 13:00:28 -0800

    chore: Add documentation for the API endpoints (#22)

--- XBOL.Ticketing/XBOL.Ticketing.API/Controllers/VenueMapsController.cs
+++ XBOL.Ticketing/XBOL.Ticketing.API/Controllers/VenueMapsController.cs
@@ -17,16 +17,34 @@ namespace XBOL.Ticketing.API.Controllers
             _venueMapService = venueMapService;
         }
 
+        /// <summary>
+        /// Retrieves a collection of venue maps available in the system.
+        /// </summary>
+        /// <remarks>This method is intended for use in a web API context and asynchronously calls the
+        /// underlying venue map service to obtain the data. The response will contain an empty list if no venue maps
+        /// are available.</remarks>
+        /// <returns>An HTTP response containing a list of objects that represent the available
+        /// venue maps.</returns>
         [HttpGet]
         [EndpointName("GetVenueMapsAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<VenueMapListItem>))]
         public async Task<ActionResult<List<VenueMapListItem>>> GetVenueMapsAsync()
         {
             var venueMaps = await _venueMapService.GetVenueMapsAsync();
             return Ok(venueMaps);
         }
 
+        /// <summary>
+        /// Retrieves the venue map associated with the specified identifier.
+        /// </summary>
+        /// <remarks>This method asynchronously fetches the venue map from the service. If no venue map is
+        /// found for the given identifier, a 404 Not Found response is returned.</remarks>
+        /// <param name="venueMapId">The unique identifier of the venue map to retrieve. Must be a positive long value.</param>
+        /// <returns>An ActionResult containing a VenueMapListItem if found; otherwise, returns a NotFound result.</returns>
         [HttpGet("{venueMapId}")]
         [EndpointName("GetVenueMapByIdAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VenueMapListItem))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<VenueMapListItem>> GetVenueMapByIdAsync([FromRoute] long venueMapId)
         {
             var result = await _venueMapService.GetVenueMapByIdAsync(venueMapId);
--- XBOL.Ticketing/XBOL.Ticketing.API/Controllers/VenuesController.cs
+++ XBOL.Ticketing/XBOL.Ticketing.API/Controllers/VenuesController.cs
@@ -17,8 +17,17 @@ namespace XBOL.Ticketing.API.Controllers
             _venueService = venueService;
         }
 
+        /// <summary>
+        /// Retrieves a list of available venues asynchronously.
+        /// </summary>
+        /// <remarks>This method is intended for scenarios where venue data is required. It performs a
+        /// non-blocking operation by leveraging the underlying venue service to fetch venue information
+        /// asynchronously.</remarks>
+        /// <returns>An object containing a list of objects that represent the
+        /// available venues. The list will be empty if no venues are found.</returns>
         [HttpGet]
         [EndpointName("GetVenuesAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<VenueListItem>))]
         public async Task<ActionResult<List<VenueListItem>>> GetVenuesAsync()
         {
             var venues = await _venueService.GetVenuesAsync();
