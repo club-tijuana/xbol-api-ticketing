094c1eed67fa78c9c3fa7ca6ae237b3a4acd4088
Carlos Rosquillas <crosquillas@gmail.com>
2026-06-18 01:49:40 -0700

    chore(booking): use shared email contracts
    Reference Odasoft.XBOL.Commons from services and tests, remove duplicate Ticketing email contract copies, and commit the regenerated appsettings schema.

--- XBOL.Ticketing/XBOL.Ticketing.Core.Commons/Email/IEmailJob.cs
+++
@@ -1,9 +0,0 @@
-using XBOL.Ticketing.Core.Commons.Request;
-
-namespace XBOL.Ticketing.Core.Commons.Email
-{
-    public interface IEmailJob
-    {
-        Task SendOrderConfirmationAsync(OrderEmailModel model);
-    }
-}
